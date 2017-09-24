2e2e07da9db1e54a1396ac76162800cd4d1dca43
Vidyashalini <32209580+Vidyashalini@users.noreply.github.com>
2017-09-25 09:01:43 +1000

    Add files via upload

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -10,19 +10,10 @@ import 'rxjs/add/operator/catch'
 export class WeatherService {
 
   constructor(private http:Http) { }
-
-  private weatherUrl = 'http://localhost:4200/data/books.json';
-
+  
   getWeather(city:string){
-    // return this.http.get(this.weatherUrl)
-    // .map(res =>  res.json().data)
-    // .do(data => console.log(data)) // eyeball results in the console
-    // .catch(this.handleError);
-    
-    return this.http.get('https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='+'"'+city+'"'+')&format=json&env=store://datatables.org/alltableswithkeys')
+     return this.http.get('https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='+'"'+city+'"'+')&format=json&env=store://datatables.org/alltableswithkeys')
         .map(response => response.json());
-   
-    
   }
 
   private handleError (error: Response) {
